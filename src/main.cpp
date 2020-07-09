d9e77d3604ad6c3c84b8dfbb90b93ffc014559f4
pavel <pavel.pugachev.94@mail.ru>
2020-07-09 14:11:46 +0300

    work with py client

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,17 @@
+// TCP сервер, возвращает полученные данные
 #include <SPI.h>
 #include <UIPEthernet.h>
 
 // определяем конфигурацию сети
 byte mac[] = {0xAE, 0xB2, 0x26, 0xE4, 0x4A, 0x5C}; // MAC-адрес
 byte ip[] = {192, 168, 0, 110}; // IP-адрес
-byte myDns[] = {192, 168, 0, 1}; // адрес DNS-сервера
+byte myDns[] = {192, 168,0, 1}; // адрес DNS-сервера
 byte gateway[] = {192, 168, 0, 1}; // адрес сетевого шлюза
 byte subnet[] = {255, 255, 255, 0}; // маска подсети
 
 EthernetServer server(2000); // создаем сервер, порт 2000
 EthernetClient client; // объект клиент
-boolean clientAlreadyConnected = false; // признак клиент уже подключен
+boolean clientAlreadyConnected= false; // признак клиент уже подключен
 
 void setup() {
     Ethernet.begin(mac, ip, myDns, gateway, subnet); // инициализация контроллера
@@ -22,10 +23,12 @@ void setup() {
 
 String reqStr;
 size_t reqSize;
-uint8_t *reqBuf;
+uint8_t* reqBuf;
 
 String respStr;
-int respCounter = 8;
+size_t respSize;
+//char* respBuf;
+uint8_t* respBuf;
 
 
 void loop() {
@@ -38,34 +41,37 @@ void loop() {
             // сообщение о подключении
             Serial.println("Client connected");
             client.println("Server ready"); // ответ клиенту
-            clientAlreadyConnected = true;
+            clientAlreadyConnected= true;
         }
 
-        while ((reqSize = client.available()) > 0) {
-            reqBuf = (uint8_t *) malloc(reqSize);
+        while((reqSize = client.available()) > 0)
+        {
+            reqBuf = (uint8_t*)malloc(reqSize);
             reqSize = client.read(reqBuf, reqSize);
 //            client.write(reqBuf,reqSize);
-            reqStr = (char *) reqBuf;
+            reqStr = (char*)reqBuf;
             reqStr = reqStr.substring(0, reqSize);
             free(reqBuf);
         }
         Serial.println(reqStr);
-        respCounter++;
 
-        String resp = "a1b2c3d";
 
-        respStr = resp;
-//        respStr = resp + respCounter+"e123\n";
-        Serial.println(respStr);
-        Serial.println("--- 1 ---");
+        respStr = "a1b2c3d";
 
-        uint8_t* dataArray = (uint8_t *) malloc(respStr.length()+1);
-        respStr.getBytes(dataArray, respStr.length()+1);
-        client.write("1\n", 2);
-//        client.write(dataArray, respStr.length()+1);
-        free(dataArray);
+//        uint8_t dataArray[dataString.length()];
+//        dataString.toCharArray(dataArray, dataString.length())
 
-        Serial.println(respStr);
-        Serial.println("--- 2 ---");
+        uint8_t dataArray[respStr.length()];
+        respStr.getBytes(dataArray, respStr.length());
+
+//        respStr.getBytes(respBuf, respStr.length());
+        client.write(dataArray, respStr.length());
+        free(respBuf);
+
+//        while(client.available() > 0) {
+//            char chr = client.read(); // чтение символа
+//            server.write(chr); // передача клиенту
+//            Serial.write(chr);
+//        }
     }
 }
